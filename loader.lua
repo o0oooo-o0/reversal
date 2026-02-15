8159aa29e0c56d9199474b233236e03cfed1680e
o0oooo-o0 <jznizr@gmail.com>
2026-02-15 12:06:08 -0500

    loader
    loider

---
+++ loader.lua
@@ -0,0 +1,31 @@
+--[[
+                                                                                        
+                                                                                    88  
+                                                                                    88  
+                                                                                    88  
+8b,dPPYba,   ,adPPYba,  8b       d8   ,adPPYba,  8b,dPPYba,  ,adPPYba,  ,adPPYYba,  88  
+88P'   "Y8  a8P_____88  `8b     d8'  a8P_____88  88P'   "Y8  I8[    ""  ""     `Y8  88  
+88          8PP"""""""   `8b   d8'   8PP"""""""  88           `"Y8ba,   ,adPPPPP88  88  
+88          "8b,   ,aa    `8b,d8'    "8b,   ,aa  88          aa    ]8I  88,    ,88  88  
+88           `"Ybbd8"'      "8"       `"Ybbd8"'  88          `"YbbdP"'  `"8bbdP"Y8  88  
+                                                                                        
+                                                                                        
+
+the loader yessssssssss 
+coded by @1.21.7 [f3kel666] on discord
+W noxo
+W sijah
+
+uses parts of my old loader
+
+
+]]
+
+if getgenv().reversal then return end
+getgenv().reversal = true
+
+loadstring(game:HttpGet(
+    game.PlaceId == 79546208627805 and "https://raw.githubusercontent.com/o0oooo-o0/reversal/refs/heads/main/99NITF_Reversal.lua" -- 99n placeid
+    or game.PlaceId == 0 and "nil"
+    or "https://raw.githubusercontent.com/o0oooo-o0/6hxForPrisonLife/refs/heads/master/unsupported.lua" -- kick if unsupported
+))()
